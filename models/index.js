50a63aa2601befcc39828b89430a7192b9c8fa2f
Zacharyjg <guldb016@umn.edu>
2021-07-20 20:03:33 -0500

    Completed relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  },
+  as: "product_to_tag"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  },
+  as: "tag_to_product"
+});
 
 module.exports = {
   Product,
