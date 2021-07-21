eb3bd3e08bf077b1e65e741bbce3de11d2c2da3f
Zacharyjg <guldb016@umn.edu>
2021-07-20 22:50:16 -0500

    Changed alias names to be more appropriate

--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ Product.belongsToMany(Tag, {
   through: {
     model: ProductTag
   },
-  as: "product_tags"
+  as: "tags"
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -27,7 +27,7 @@ Tag.belongsToMany(Product, {
   through: {
     model: ProductTag
   },
-  as: "tag_to_product"
+  as: "products"
 });
 
 module.exports = {
